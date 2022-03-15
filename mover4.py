933169ae8a607dd88ac818811591d91598d3503c
superchargez <87804931+superchargez@users.noreply.github.com>
2022-03-15 13:00:24 +0500

    Update mover4.py

--- mover4.py
+++ mover4.py
@@ -4,5 +4,5 @@ with open("filename.txt") as file:
 	    lines = [line.rstrip() for line in file]
 
 for f in os.listdir('.'):
-    if any(fnmatch.fnmatch(f, pat+'*') for pat in calls):
-        shutil.move(f, 'NDFCalls')
+    if any(fnmatch.fnmatch(f, pat+'*') for pat in lines):
+        shutil.move(f, 'DestinationFolder')
